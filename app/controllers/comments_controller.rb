60f8123cc42e685800607b1b05722a9142c4e027
Your Name <you@example.com>
2024-02-19 21:25:56 +0000

    Add comment working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
   def create
+    #query_venue_id=28&query_author_id=me&query_body=wow.+
+    
     comment = Comment.new
     comment.author_id = params.fetch("query_author_id")
     comment.venue_id = params.fetch("query_venue_id")
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,3 +41,5 @@ class UsersController < ApplicationController
   end
 
 end
+  
+
